4933651f29866ef0b7733cba44346675035e5ba3
Mike Sammartino <sammartino.mike@gmail.com>
2025-07-31 12:06:44 -0500

    make __dirname available in ES module context, turn html reporter on

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, devices } from '@playwright/test'
+import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// Make __dirname available in an ES module
+const file = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(file)
+
+dotenv.config({ path: path.resolve(__dirname, '.env') })
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -22,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]],
+  reporter: [['html', { open: 'on-failure' }]],
   testIgnore: ['e2e/app.spec.ts', 'e2e/example.spec.ts'],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {

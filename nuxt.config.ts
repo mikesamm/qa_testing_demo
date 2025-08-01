bab31a106049544bcbecd3c4cd3f263a1adec127
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-01 08:00:18 -0500

    turn color-mode off to avoid extra overlays

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/test-utils/module'],
   css: ['../assets/css/main.css'],
+  ui: {
+    colorMode: false,
+  },
 })

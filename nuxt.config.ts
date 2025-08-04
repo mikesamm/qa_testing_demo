67c31ab2772476a968d569e8323862eb98e76465
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-04 16:33:27 -0500

    add and config @nuxt/content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/test-utils/module'],
+  modules: ['@nuxt/ui', '@nuxt/test-utils/module', '@nuxt/content'],
   css: ['../assets/css/main.css'],
 })

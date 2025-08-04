d80704733124aae6b5519b02157e4ec9429550cf
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-04 13:44:12 -0500

    add link to github repo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h1>Welcome to the Dashboard</h1>
+    <h3>
+      Check out the source files
+      <a href="https://github.com/mikesamm/qa_testing_demo">here</a>.
+    </h3>
+    <h1>Dashboard</h1>
     <OnOffSwitch />
   </div>
 </template>

a438f296d2f26421788e8635e8b24e07f9cd2126
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-04 17:26:02 -0500

    update css import, render page links for headings in markdown content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/test-utils/module', '@nuxt/content'],
-  css: ['../assets/css/main.css'],
+  css: ['./assets/css/main.css'],
+  content: {
+    renderer: {
+      anchorLinks: {
+        h1: true,
+        h2: true,
+        h3: true,
+        h4: true,
+      },
+    },
+  },
 })

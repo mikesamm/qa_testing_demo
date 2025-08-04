9fa26353280b483b5268450bae166c179427a2f5
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-04 16:33:50 -0500

    init NavMenu

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <template>
   <UApp>
+    <NavMenu />
     <NuxtPage />
   </UApp>
 </template>
---
+++ app/components/NavMenu.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: 'Dashboard',
+    icon: 'i-lucide-book-open',
+    to: '/',
+  },
+  {
+    label: 'Strategy',
+    icon: 'i-lucide-database',
+    to: '/strategy',
+  },
+  {
+    label: 'Reports',
+    icon: 'i-lucide-box',
+    to: '/reports',
+  },
+  {
+    label: 'GitHub',
+    icon: 'i-lucide-github',
+    to: 'https://github.com/mikesamm/qa_testing_demo',
+    target: '_blank',
+  },
+])
+</script>
+
+<template>
+  <UNavigationMenu
+    :items="items"
+    class="w-full justify-center"
+  />
+</template>

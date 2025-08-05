eb4d60f2534f4287c6ab9c1db1f6d889d2643446
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-04 20:29:54 -0500

    init DimmerSwitch and MultiSwitch

---
+++ app/components/DimmerSwitch.vue
@@ -0,0 +1,12 @@
+<template>
+  <USlider
+    v-model="intensity"
+    default-value="0"
+    orientation="vertical"
+    class="h-48"
+  />
+</template>
+
+<script setup lang="ts">
+const intensity = ref(0)
+</script>
---
+++ app/components/MultiSwitch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <UButtonGroup
+      class="max-w-32px"
+      orientation="vertical"
+      size="lg"
+    >
+      <UButton
+        v-for="(label, index) in labels"
+        :key="index"
+        variant="subtle"
+        :label="label"
+      />
+    </UButtonGroup>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { labels } = defineProps<{
+  labels: string[]
+}>()
+</script>

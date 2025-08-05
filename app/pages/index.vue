8a6513b0a75182207280b3385962d4de5f4e010b
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-04 20:35:47 -0500

    add device skeletons and preliminary flexbox layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
+  <div
+    class="flex flex-col w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8"
+  >
     <h1>Dashboard</h1>
-    <OnOffSwitch />
+    <div class="flex flex-row justify-between mb-4">
+      <div id="big-single-light"></div>
+      <div id="multi-lights">
+        <div class="single-light"></div>
+        <div class="single-light"></div>
+        <div class="single-light"></div>
+      </div>
+      <div id="outlet"></div>
+      <div id="breakers">
+        <div class="circuit-breaker"></div>
+        <div class="circuit-breaker"></div>
+        <div class="circuit-breaker"></div>
+      </div>
+    </div>
+    <div class="flex flex-row justify-between">
+      <DimmerSwitch />
+      <MultiSwitch :labels="['Morning', 'Cooking', 'Reading']" />
+      <OnOffSwitch />
+      <MultiSwitch :labels="['1', '2', '3']" />
+    </div>
   </div>
 </template>

2085a92fbe6aa2daecc506e1ca3b718fabb3d1ad
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-06 15:03:51 -0500

    add tables of test cases

--- app/pages/testSuite.vue
+++ app/pages/testSuite.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Test Suites</h1>
+  <h1 class="project-title">Test Suites</h1>
+  <UTable :data="tc_dashboardUI" />
+  <UTable :data="tc_onOffSwitch" />
+  <UTable :data="tc_dimmerSwitch" />
+  <UTable :data="tc_sceneSelectSwitch" />
+  <UTable :data="tc_breakerSwitches" />
 </template>
+
+<script setup lang="ts">
+import tc_dashboardUI from '../assets/data/tc-dashboardui.json'
+import tc_onOffSwitch from '../assets/data/tc-onoffswitch.json'
+import tc_dimmerSwitch from '../assets/data/tc-dimmerswitch.json'
+import tc_sceneSelectSwitch from '../assets/data/tc-sceneselect.json'
+import tc_breakerSwitches from '../assets/data/tc-breakerswitches.json'
+</script>

b9e46b92593f97c729946a9f5bbb11e172c6c1b6
Mike Sammartino <sammartino.mike@gmail.com>
2025-08-06 17:10:17 -0500

    add border for light mode visibility

--- app/components/DimmerSwitch.vue
+++ app/components/DimmerSwitch.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col items-center space-y-4">
-    <div id="big-single-light"></div>
+    <div
+      id="big-single-light"
+      :style="{ border: '1px solid', borderColor: borderColor }"
+    ></div>
     <USlider
       v-model="intensity"
       orientation="vertical"
@@ -12,4 +15,9 @@
 
 <script setup lang="ts">
 const intensity = ref(0)
+const colorMode = useColorMode()
+
+const borderColor = computed(() => {
+  return colorMode.value === 'dark' ? 'white' : '#0f172b'
+})
 </script>
